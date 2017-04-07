8f83baa503be4264b7138b91efbdb9c35aa94149
Shane Tajima <shane@meyercomputer.com>
2017-04-06 16:24:36 -1000

    modded

--- temp/appSetup.js
+++ temp/appSetup.js
@@ -23,86 +23,101 @@ function loadScript(url, callback) {
 }
 
 var scriptsLoaded = 0;
-var totalScripts = 12;
+var totalScripts = 10;
+var vendorDone = false;
+var polyDone = false;
 var domain = "https://rawgit.com/stajima/ng-admin/master/dist";
 function scripts() {
-    loadScript(domain + '/vendor.dll.js', function () {
-        console.log('vendor dll loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/vendor.bundle.js', function () {
-        console.log('vendor bundle loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/0.chunk.js', function () {
-        console.log('chunk 0 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/1.chunk.js', function () {
-        console.log('chunk 1 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/2.chunk.js', function () {
-        console.log('chunk 2 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/3.chunk.js', function () {
-        console.log('chunk 3 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/4.chunk.js', function () {
-        console.log('chunk 4 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/5.chunk.js', function () {
-        console.log('chunk 5 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/6.chunk.js', function () {
-        console.log('chunk 6 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/7.chunk.js', function () {
-        console.log('chunk 7 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/8.chunk.js', function () {
-        console.log('chunk 8 loaded');
-        scriptsLoaded++;
-        launch();
-    });
-    loadScript(domain + '/9.chunk.js', function () {
-        console.log('chunk 9 loaded');
-        scriptsLoaded++;
-        launch();
-    });
+    if (vendorDone && polyDone) {
+        loadScript(domain + '/0.chunk.js', function () {
+            console.log('chunk 0 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/1.chunk.js', function () {
+            console.log('chunk 1 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/2.chunk.js', function () {
+            console.log('chunk 2 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/3.chunk.js', function () {
+            console.log('chunk 3 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/4.chunk.js', function () {
+            console.log('chunk 4 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/5.chunk.js', function () {
+            console.log('chunk 5 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/6.chunk.js', function () {
+            console.log('chunk 6 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/7.chunk.js', function () {
+            console.log('chunk 7 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/8.chunk.js', function () {
+            console.log('chunk 8 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+        loadScript(domain + '/9.chunk.js', function () {
+            console.log('chunk 9 loaded');
+            scriptsLoaded++;
+            launch();
+        });
+    }
 };
 
-(function loadPolyDll() {
+(function init() {
+    loadPolyDll();
+    loadVendorDll();
+})();
+
+function loadPolyDll() {
     loadScript(domain + '/polyfills.dll.js', function () {
         console.log('polyfills dll loaded');
         loadPolyBundle();
     });
-})();
+};
 
 function loadPolyBundle() {
     loadScript(domain + '/polyfills.bundle.js', function () {
-        console.log('init ployfills bundle loaded');
+        console.log('ployfills bundle loaded');
+        polyDone = true;
         scripts();
     });
 };
 
-var launch = function () {
+function loadVendorDll() {
+    loadScript(domain + '/vendor.dll.js', function () {
+        console.log('vendor dll loaded');
+        loadVendorBundle();
+    });
+};
+
+function loadVendorBundle() {
+    loadScript(domain + '/vendor.bundle.js', function () {
+        console.log('vendor bundle loaded');
+        vendorDone = true;
+        scripts();
+    });
+};
+
+function loadMain() {
     if (scriptsLoaded === totalScripts) {
         loadScript(domain + '/main.bundle.js', function () {
             console.log('main bundle loaded');
