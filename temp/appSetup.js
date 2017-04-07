d78c5db129073bab0f2fc9ecbad56b7a32ae08cc
Shane Tajima <shane@meyercomputer.com>
2017-04-07 09:28:24 -1000

    index changed. appsetup script load style changed

--- temp/appSetup.js
+++ temp/appSetup.js
@@ -1,27 +1,10 @@
 console.log('appSetup.js');
-//TODO change to use loader style load with all async. test on index first.
+//TODO change to use loader style load
 function loadScript(url, callback) {
-
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.defer = true;
-
-    if (script.readyState) {  //IE
-        script.onreadystatechange = function () {
-            if (script.readyState == "loaded" ||
-                script.readyState == "complete") {
-                script.onreadystatechange = null;
-                callback();
-            }
-        };
-    } else {  //Others
-        script.onload = function () {
-            callback();
-        };
-    }
-
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
+    var scriptElement = document.createElement("script");
+    scriptElement.src = url;
+    scriptElement.onload = callback || null;
+    document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
 }
 
 var scriptsLoaded = 0;
