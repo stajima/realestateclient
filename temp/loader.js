15ccb65679d8ff902df8f4d3bb27b49fd2528286
Shane Tajima <shane@meyercomputer.com>
2017-04-07 13:55:28 -1000

    index changed. appsetup script load style changed. loader udpated

--- temp/loader.js
+++ temp/loader.js
@@ -1,8 +1,8 @@
 console.log('loader.js')
-ramdidx4.scriptElement = document.createElement("script");
-ramdidx4.scriptElement.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+ramidx4.scriptElement = document.createElement("script");
+ramidx4.scriptElement.src = "https://code.jquery.com/jquery-3.2.1.min.js";
 document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
 
-ramdidx4.scriptElement = document.createElement("script");
-ramdidx4.scriptElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
+ramidx4.scriptElement = document.createElement("script");
+ramidx4.scriptElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
 document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
