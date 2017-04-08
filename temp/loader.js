adc9d5c21a0ba3712c21c59695dc742e5481eadc
Shane Tajima <shane@meyercomputer.com>
2017-04-07 14:10:31 -1000

    loader updated. attemping to fix load order

--- temp/loader.js
+++ temp/loader.js
@@ -1,9 +1,34 @@
 console.log('loader.js')
-var scriptElement = document.createElement("script");
-scriptElement.src = "https://code.jquery.com/jquery-3.2.1.min.js";
-document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
+this.loadScript = function (url, callback) {
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            callback();
+        };
+    }
 
-var scriptElement = document.createElement("script");
-scriptElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
-// scriptElement.onload = gotApp;
-document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
+    script.src = url;
+    document.body.insertBefore(script, document.getElementById("ramidx4_loader"));
+}
+
+loadScript("https://code.jquery.com/jquery-3.2.1.min.js", function () {
+    var scriptElement = document.createElement("script");
+    scriptElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
+    document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
+});
+
+// var scriptElement = document.createElement("script");
+// scriptElement.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+// document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
+
+// var scriptElement = document.createElement("script");
+// scriptElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
+// document.body.insertBefore(scriptElement, document.getElementById("ramidx4_loader"));
