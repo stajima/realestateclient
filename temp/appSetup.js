fbebd7aa6ee8efb2e5caf7aaf2a03e1d8ef8bacb
Shane Tajima <shane@meyercomputer.com>
2017-04-27 09:47:25 -1000

    IIFEs used now. Inline scripts removed.

--- temp/appSetup.js
+++ temp/appSetup.js
@@ -1,28 +1,28 @@
 console.log('appSetup.js');
 
-var domain = "https://rawgit.com/stajima/ng-admin/master/dist";
+(function () {
+    var domain = "https://rawgit.com/stajima/ng-admin/master/dist";
+    var parentNode = document.getElementById("ramidx4_loader").parentNode;
 
-var scriptElement = document.createElement("link");
-scriptElement.rel = "stylesheet";
-scriptElement.href = domain + "/initial.css";
-document.getElementsByTagName('head')[0].appendChild(scriptElement);
+    var inlineElement = document.createElement("script");
+    inlineElement.src = domain + '/inline.bundle.js';
 
-var polyElement = document.createElement("script");
-polyElement.src = domain + '/polyfills.bundle.js';
-polyElement.defer = true;
-var parentNode = document.getElementById("ramidx4_loader").parentNode;
+    var polyElement = document.createElement("script");
+    polyElement.src = domain + '/polyfills.bundle.js';
 
-var vendorElement = document.createElement("script");
-vendorElement.src = domain + '/vendor.bundle.js';
-vendorElement.defer = true;
-var parentNode = document.getElementById("ramidx4_loader").parentNode;
+    var stylesElement = document.createElement("script");
+    stylesElement.src = domain + '/styles.bundle.js';
 
-var mainElement = document.createElement("script");
-mainElement.src = domain + '/main.bundle.js';
-mainElement.defer = true;
-var parentNode = document.getElementById("ramidx4_loader").parentNode;
+    var vendorElement = document.createElement("script");
+    vendorElement.src = domain + '/vendor.bundle.js';
+
+    var mainElement = document.createElement("script");
+    mainElement.src = domain + '/main.bundle.js';
 
 
-parentNode.insertBefore(polyElement, document.getElementById("ramidx4_loader"));
-parentNode.insertBefore(vendorElement, document.getElementById("ramidx4_loader"));
-parentNode.insertBefore(mainElement, document.getElementById("ramidx4_loader"));
+    parentNode.insertBefore(inlineElement, document.getElementById("ramidx4_loader"));
+    parentNode.insertBefore(polyElement, document.getElementById("ramidx4_loader"));
+    parentNode.insertBefore(stylesElement, document.getElementById("ramidx4_loader"));
+    parentNode.insertBefore(vendorElement, document.getElementById("ramidx4_loader"));
+    parentNode.insertBefore(mainElement, document.getElementById("ramidx4_loader"));
+})();
--- temp/loader.js
+++ temp/loader.js
@@ -1,12 +1,14 @@
 console.log('loader.js')
 
-var jqueryElement = document.createElement("script");
-jqueryElement.src = "https://code.jquery.com/jquery-3.2.1.min.js";
-var parentNode = document.getElementById("ramidx4_loader").parentNode;
-parentNode.insertBefore(jqueryElement, document.getElementById("ramidx4_loader"));
+(function () {
+    var jqueryElement = document.createElement("script");
+    jqueryElement.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+    var parentNode = document.getElementById("ramidx4_loader").parentNode;
+    parentNode.insertBefore(jqueryElement, document.getElementById("ramidx4_loader"));
 
-var appSetupElement = document.createElement("script");
-appSetupElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
-var parentNode = document.getElementById("ramidx4_loader").parentNode;
-parentNode.insertBefore(appSetupElement, document.getElementById("ramidx4_loader"));
+    var appSetupElement = document.createElement("script");
+    appSetupElement.src = "https://stajima.github.io/realestateclient/temp/appSetup.js";
+    var parentNode = document.getElementById("ramidx4_loader").parentNode;
+    parentNode.insertBefore(appSetupElement, document.getElementById("ramidx4_loader"));
 
+})();
